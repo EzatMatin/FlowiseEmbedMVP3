592b4d0a3591f76ddc0ce0b4ced649eb5ef5caa5
ezatullah Matin Wakily <78909369+EzatMatin@users.noreply.github.com>
2024-08-14 19:03:46 -0300

    uijhn

--- src/components/bubbles/StarterPromptBubble.tsx
+++ src/components/bubbles/StarterPromptBubble.tsx
@@ -13,7 +13,7 @@ export const StarterPromptBubble = (props: Props) => (
       onClick={() => props.onPromptClick?.()}
     >
       <span
-        class="relative flex w-40 flex-col gap-2 rounded-2xl border border-token-border-light px-3 pb-4 pt-3 text-start align-top text-[15px] shadow-xxs transition enabled:hover:bg-token-main-surface-secondary disabled:cursor-not-allowed"
+        class="relative flex w-auto flex-col gap-2 rounded-2xl border border-token-border-light px-3 pb-4 pt-3 text-start align-top text-[15px] shadow-xxs transition enabled:hover:bg-token-main-surface-secondary disabled:cursor-not-allowed"
         data-testid="host-bubble"
         style={{
           width: 'max-content',
